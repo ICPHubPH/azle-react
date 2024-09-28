78951826518aa1ef4cad3a5f1ff1a7981ecee98a
John Andrei Tacujan <89928907+DreiDev04@users.noreply.github.com>
2024-09-28 07:54:40 +0000

    Added: Front end form

--- src/backend/app/Controllers/Http/ApisController.ts
+++ src/backend/app/Controllers/Http/ApisController.ts
@@ -1,73 +1,76 @@
-import { Configuration } from 'Database/entities/configuration';
-import { Response, Request } from 'express';
+import { Configuration } from "Database/entities/configuration";
+import { Response, Request } from "express";
 
 export default class ApisController {
-    static async greet(request: Request, response: Response) {
-        response.json({ greeting: `Hello, ${request.query.name}` });
+  
+
+
+  static async greet(request: Request, response: Response) {
+    response.json({ greeting: `Hello, ${request.query.name}` });
+  }
+
+  static async configurations(request: Request, response: Response) {
+    const configuration = await Configuration.find();
+
+    response.json({
+      status: 1,
+      data: configuration,
+    });
+  }
+
+  static async insert_configuration(request: Request, response: Response) {
+    const { key, value } = request.body;
+    await Configuration.insert({ key, value });
+
+    const checkIfExist = await Configuration.findBy({ key });
+
+    if (!checkIfExist) {
+      response.json({
+        status: 0,
+        message: "Configuration already exists!",
+      });
     }
 
-    static async configurations(request: Request, response: Response){
-        const configuration = await Configuration.find();
+    response.json({
+      status: 1,
+      message: "Configuration has been inserted!",
+    });
+  }
 
-        response.json({
-            status: 1,
-            data: configuration
-        });
+  static async update_configuration(request: Request, response: Response) {
+    const { key, value } = request.body;
+    const getConfiguration = await Configuration.findBy({ key });
+
+    if (!getConfiguration) {
+      response.json({
+        status: 0,
+        message: "Configuration not found!",
+      });
     }
 
-    static async insert_configuration(request: Request, response: Response){
-        const { key, value } = request.body;
-        await Configuration.insert({key, value});
+    await Configuration.update({ key }, { value });
+    response.json({
+      status: 1,
+      message: "Configuration has been updated!",
+    });
+  }
 
-        const checkIfExist = await Configuration.findBy({ key });
+  static async delete_configuration(request: Request, response: Response) {
+    const { key } = request.body;
+    const getConfiguration = await Configuration.findBy({ key });
 
-        if(!checkIfExist){
-            response.json({
-                status: 0,
-                message: "Configuration already exists!"
-            });
-        }
-
-        response.json({
-            status: 1,
-            message: "Configuration has been inserted!",
-        });
+    if (!getConfiguration) {
+      response.json({
+        status: 0,
+        message: "Configuration not found!",
+      });
     }
 
-    static async update_configuration(request: Request, response: Response){
-        const { key, value } = request.body;
-        const getConfiguration = await Configuration.findBy({ key });
+    await Configuration.delete({ key });
 
-        if(!getConfiguration){
-            response.json({
-                status: 0,
-                message: "Configuration not found!"
-            });
-        }
-        
-        await Configuration.update({ key }, { value });
-        response.json({
-            status: 1,
-            message: "Configuration has been updated!",
-        });
-    }
-
-    static async delete_configuration(request: Request, response: Response){
-        const { key } = request.body;
-        const getConfiguration = await Configuration.findBy({ key });
-
-        if(!getConfiguration){
-            response.json({
-                status: 0,
-                message: "Configuration not found!"
-            });
-        }
-
-        await Configuration.delete({ key });
-
-        response.json({
-            status: 1,
-            message: "Configuration has been deleted!",
-        });
-    }
+    response.json({
+      status: 1,
+      message: "Configuration has been deleted!",
+    });
+  }
 }
--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,29 +1,131 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { FormInputIcon } from "lucide-react";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+});
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [greeting, setGreeting] = useState("");
 
-  function handleSubmit(event: any) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    fetch(`${import.meta.env.VITE_CANISTER_URL}/greet?name=${name}`)
-      .then(response => response.json()).then((json) => {
-        setGreeting(json.greeting)
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+      email: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    const { username, email, password } = values;
+    fetch(`${import.meta.env.VITE_CANISTER_URL}/greet?name=${username}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setGreeting(json.greeting);
       });
+    console.log(values);
   }
 
+  // function handleSubmit(event: any) {
+  //   event.preventDefault();
+  //   const name = event.target.elements.name.value;
+  //   fetch(`${import.meta.env.VITE_CANISTER_URL}/greet?name=${name}`)
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setGreeting(json.greeting);
+  //     });
+  // }
+
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
+    <main className="flex flex-col justify-center items-center h-screen w-screen">
+      {/* <form action="#" onSubmit={handleSubmit}>
         <label htmlFor="name">Enter your name: &nbsp;</label>
         <input id="name" alt="Name" type="text" />
-        <button type="submit">Hey hey!</button>
+        <Button type="submit" className="bg-red-500">
+          Hey hey!
+        </Button>
       </form>
-      <section id="greeting">{greeting}</section>
-    </main >
+      <section id="greeting">{greeting}</section> */}
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className=" bg-secondary p-8 rounded-lg min-w-[300px]"
+        >
+          <h1 className="text-xl font-bold font-mono text-center mb-5">
+            Welcome to DFINITY
+          </h1>
+
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input {...field} autoComplete="off" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} type="email" autoComplete="off" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input {...field} type="password" autoComplete="off" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-end mt-5">
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </Form>
+    </main>
   );
 }
 
