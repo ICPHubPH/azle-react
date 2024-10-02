5c268089b1ef42edfae40279735bc8fafb3a7922
xyugen <alex.arias.jorym@gmail.com>
2024-10-02 09:37:07 +0000

    feat: initialize tailwind css

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import environment from 'vite-plugin-environment';
 import dotenv from 'dotenv';
+import path from "path";
 
 dotenv.config({ path: '../../.env' });
 
@@ -38,6 +39,10 @@ export default defineConfig({
           new URL("../declarations", import.meta.url)
         ),
       },
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./src"),
+      }
     ],
   },
 });
