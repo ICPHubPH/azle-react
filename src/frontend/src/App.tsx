3f90a2164aec05e489f040f35321fb83b06ea069
xyugen <alex.arias.jorym@gmail.com>
2024-10-04 14:05:42 +0000

    feat: configure theme color variables

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,34 +1,7 @@
-import type React from "react";
-import { useState } from "react";
-
 function App() {
-	const [greeting, setGreeting] = useState("");
-
-	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-		event.preventDefault();
-		const target = event.target as HTMLFormElement;
-		const name = (target.elements.namedItem('name') as HTMLInputElement).value;
-		
-		fetch(`${import.meta.env.VITE_CANISTER_URL}/greet?name=${name}`)
-			.then((response) => response.json())
-			.then((json) => {
-				setGreeting(json.greeting);
-			});
-	}
-
 	return (
 		<main>
-			<img src="/logo2.svg" alt="DFINITY logo" />
-			<br />
-			<div>
-				<p> hello world</p>
-			</div>
-			<form action="#" onSubmit={handleSubmit}>
-				<label htmlFor="name">Enter your name: &nbsp;</label>
-				<input id="name" alt="Name" type="text" />
-				<button type="submit">Hey hey!</button>
-			</form>
-			<section id="greeting">{greeting}</section>
+			<h1>Hello World!</h1>
 		</main>
 	);
 }
--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ["class"],
-	content: [],
+	content: ["./src/**/*.{tsx,ts}"],
 	theme: {
 		extend: {
 			borderRadius: {
