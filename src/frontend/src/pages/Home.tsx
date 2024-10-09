92fd7c43db519abab91b907c9c847013dac8dd2a
Johnmack Faeldonia <johmmackfaeldonia@gmail.com>
2024-10-09 19:24:34 +0000

    feat: Improve folder structure and install Tailwind
    - Organize folder structure for better maintainability
    - Create a home page for initial testing
    - Test if Tailwind works

---
+++ src/frontend/src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from '../components/Button';
+import Header from '../components/Header';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+        <Header />
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-50 dark:bg-black">
+    
+      <h1 className="text-4xl text-blue-600 dark:text-yellow-400">Welcome to ISKOLAR PILIPINAS!</h1>
+      <p className="text-lg text-gray-800 dark:text-gray-200">Connecting students and providers for quality education.</p>
+      <Button label="Get Started" onClick={() => alert('Button clicked!')} />
+    </div>
+    </div>
+  );
+};
+
+export default Home;
