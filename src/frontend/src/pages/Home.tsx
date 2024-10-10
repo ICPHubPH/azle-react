7be3fb524fb8fa8d44bd83c726288d3ef668f106
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-10-10 11:33:02 +0800

    Install shadcn, add initial components, and modify folder structure in pages

--- src/frontend/src/pages/Home.tsx
+++ src/frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Button from '../components/Button';
+import { Button } from "@/components/ui/button"
+
 import Header from '../components/Header';
 
 const Home: React.FC = () => {
@@ -10,7 +11,11 @@ const Home: React.FC = () => {
     
       <h1 className="text-4xl text-blue-600 dark:text-yellow-400">Welcome to ISKOLAR PILIPINAS!</h1>
       <p className="text-lg text-gray-800 dark:text-gray-200">Connecting students and providers for quality education.</p>
-      <Button label="Get Started" onClick={() => alert('Button clicked!')} />
+      <Button
+  
+      variant="destructive">Button</Button>
+
+      {/* <Button label="Get Started" onClick={() => alert('Button clicked!')} /> */}
     </div>
     </div>
   );
