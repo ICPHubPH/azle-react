ded477b9bf24c14a54cb3ab1a59da4413c951ab3
RYBN <official.ravendelarama@gmail.com>
2024-10-10 19:44:48 +0000

    added initial post form

---
+++ src/frontend/src/components/post-form/create-post.tsx
@@ -0,0 +1,203 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { CirclePlus, Plus } from "lucide-react";
+import "./create-post-ql.css";
+
+import { ChangeEvent, useEffect, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
+const modules = {
+  toolbar: [
+    [
+      { size: ["small", false, "large", "huge"] },
+      { header: "1" },
+      { header: "2" },
+      { font: [] },
+    ],
+    [{ script: "sub" }, { script: "super" }], // Subscript/Superscript
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }], // Indent
+    [{ bold: true }, { italic: true }, { underline: true }, { strike: true }],
+    [{ color: [] }, { background: [] }], // Text color and background color
+    [{ align: [] }], // Text alignment
+    [{ direction: "rtl" }], // Text direction
+
+    // Custom items:
+    ["blockquote", "code-block"], // Block quote and code block
+    ["image"], // Image upload
+    ["clean"], // Clear formatting
+  ],
+};
+
+const formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "color",
+  "background",
+  "script",
+  "blockquote",
+  "code-block",
+  "list",
+  "bullet",
+  "indent",
+  "direction",
+  "align",
+  "image",
+];
+
+export default function CreatePost() {
+  const [thumbnail, setThumbnail] = useState<File | null>(null);
+  const [thumbnailURL, setThumbnailURL] = useState<string | null>(null);
+  const [title, setTitle] = useState<string>("");
+  const [type, setType] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  useEffect(() => {
+    return () => {
+      // Revoke the object URL to avoid memory leaks
+      if (thumbnailURL) {
+        URL.revokeObjectURL(thumbnailURL);
+      }
+    };
+  }, [thumbnailURL]);
+
+  function handleThumbnail(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      setThumbnail(file);
+      setThumbnailURL(URL.createObjectURL(file));
+    }
+  }
+
+  function handleTitle(e: ChangeEvent<HTMLInputElement>) {
+    setTitle(e.target.value as string);
+  }
+
+  function handlePlatformType(value: string) {
+    setType(value);
+  }
+
+  function handleContent(value: string) {
+    setContent(value);
+  }
+
+  function createPostHandler() {
+    console.log(thumbnailURL);
+    console.log(title);
+    console.log(type);
+    console.log(content);
+  }
+
+  function draftPost() {
+    localStorage.setItem("draft-platform-title", title);
+    localStorage.setItem("draft-platform-type", type);
+    localStorage.setItem("draft-platform-content", content);
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Plus />
+      </DialogTrigger>
+      <DialogContent className="overflow-y-auto no-scrollbar rounded-none md:rounded-md max-h-full md:max-h-[90%] w-full md:max-w-[50%]">
+        <DialogHeader>
+          <DialogTitle>Create new post platform</DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col gap-4">
+          <div className="rounded-lg p-5 flex flex-col justify-center items-center gap-4">
+            <Label htmlFor="thumbnail" className="flex gap-2 items-center px-4">
+              <Input
+                type="file"
+                id="thumbnail"
+                placeholder="Thumbnail"
+                accept="image/*"
+                onChange={handleThumbnail}
+                className="hidden"
+              />
+              {thumbnailURL ? (
+                <div className="w-full h-52">
+                  <img
+                    src={thumbnailURL}
+                    alt="thumbnail"
+                    className="h-full w-full rounded-sm object-contain"
+                  />
+                </div>
+              ) : (
+                <>
+                  <CirclePlus /> Upload thumbnail
+                </>
+              )}
+            </Label>
+          </div>
+
+          <div className="flex items-center gap-4 justify-start">
+            <Input
+              placeholder="Title"
+              onChange={handleTitle}
+              value={title}
+              className="w-60"
+            />
+
+            <Select onValueChange={handlePlatformType}>
+              <SelectTrigger className="w-52">
+                <SelectValue placeholder="Platform Type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Scholarships</SelectLabel>
+                  <SelectItem value="gov">Government Supported</SelectItem>
+                  <SelectItem value="school">
+                    College/University Scholarship
+                  </SelectItem>
+                  <SelectItem value="standalone">Standalone</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className="">
+            <ReactQuill
+              theme="snow"
+              value={content}
+              onChange={handleContent}
+              modules={modules}
+              formats={formats}
+              placeholder="Enter scholarship details here..."
+              className="overflow-y-auto no-scrollbar"
+            />
+          </div>
+          <div className="flex justify-end gap-2 items-center">
+            <Button variant={"default"} size={"lg"} onClick={createPostHandler}>
+              Create new post
+            </Button>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/frontend/src/pages/landing/index.tsx
+++ src/frontend/src/pages/landing/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import Header from '../../components/Header'
-import { Separator } from "@/components/ui/separator"
-import Testimonies from '@/components/landing/testimonies/Testimonies'
-import Providers from '@/components/landing/donor/Providers'
+import Providers from "@/components/landing/donor/Providers";
+import Testimonies from "@/components/landing/testimonies/Testimonies";
+import { Separator } from "@/components/ui/separator";
+import React from "react";
+import Header from "../../components/Header";
 
 const Home: React.FC = () => {
   return (
-    <div className='scroll-smooth'>
+    <div className="scroll-smooth">
       <Header />
-      <div className="flex flex-col items-center justify-center h-screen" id="home">
-        <h1 className="text-4xl md:text-5xl lg:text-7xl font-semibold text-blue-500 dark:text-yellow-500">Welcome to ISKOLAR PH!</h1>
-        <p className="text-lg text-gray-800 dark:text-gray-200">Connecting students and providers for quality education.</p>
+      <div
+        className="flex flex-col items-center justify-center h-screen"
+        id="home"
+      >
+        <h1 className="text-4xl md:text-5xl lg:text-7xl font-semibold text-blue-500 dark:text-yellow-500">
+          Welcome to ISKOLAR PH!
+        </h1>
+        <p className="text-lg text-gray-800 dark:text-gray-200">
+          Connecting students and providers for quality education.
+        </p>
       </div>
       <Separator />
       <div className="container mx-auto px-4" id="testimonies">
@@ -21,7 +28,7 @@ const Home: React.FC = () => {
         <Providers />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
