c49561065fbcc376a94b82f0a208792be0684670
xyugen <alex.arias.jorym@gmail.com>
2024-10-13 06:58:01 +0000

    feat(log): sign in with identity

--- src/frontend/src/components/SocialsAuth.tsx
+++ src/frontend/src/components/SocialsAuth.tsx
@@ -1,12 +1,26 @@
+import { AuthClient } from "@dfinity/auth-client";
 import { Button } from "./ui/button";
 import { Facebook, Google, InternetComputer } from "@/assets/icons";
 
 const SocialsAuth = () => {
+  const signInWithIdentity = async () => {
+    const authClient = await AuthClient.create();
+
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: async () => {
+        const identity = authClient.getIdentity();
+        console.log(identity);
+      },
+    })
+  };
+
 	return (
 		<div className="space-y-4">
 			<Button
 				variant={"outline"}
 				className="w-full border-indigo-500 shadow-md shadow-indigo-200"
+        onClick={signInWithIdentity}
 			>
 				<InternetComputer className="size-6 p-1 bg-white rounded-full mr-2" />
 				Continue with Internet Identity
