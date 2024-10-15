9c40c254e287369222d5cac5ca22867bed76000a
Renz Arias <68522615+xyugen@users.noreply.github.com>
2024-10-15 14:52:40 +0000

    chore(auth): remove Google and Facebook social authentication

--- src/frontend/src/components/SocialsAuth.tsx
+++ src/frontend/src/components/SocialsAuth.tsx
@@ -25,14 +25,14 @@ const SocialsAuth = () => {
 				<InternetComputer className="size-6 p-1 bg-white rounded-full mr-2" />
 				Continue with Internet Identity
 			</Button>
-			<Button variant={"outline"} className="w-full">
+			{/* <Button variant={"outline"} className="w-full">
 				<Google className="size-6 p-1 bg-white rounded-full mr-2" />
 				Continue with Google
 			</Button>
 			<Button variant={"outline"} className="w-full">
 				<Facebook className="size-6 p-1 bg-white rounded-full mr-2" />
 				Continue with Facebook
-			</Button>
+			</Button> */}
 		</div>
 	);
 };
