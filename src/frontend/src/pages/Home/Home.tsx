38ab1b72347b2535c2911e03313b7d49bf1b1711
Paula <peaksenut@gmail.com>
2024-10-15 05:45:19 +0000

    ADD: fake api to fetch cards

--- src/frontend/src/pages/Home/Home.tsx
+++ src/frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,25 @@
+import useFlashcardStore from "@/store/useFlashcardStore";
 import Reels from "./_components/Reels/Reels";
+import React from "react";
 
 function Home() {
+  const { cards, redoCards } = useFlashcardStore();
+  const getCards = useFlashcardStore((state) => state.getCards);
+
+  React.useEffect(() => {
+    getCards();
+  }, [getCards]);
+
   return (
     <div>
+<<<<<<< Updated upstream
       <Reels />
+=======
+      <Reels
+        cards={cards}
+        redoCards={redoCards}
+      />
+>>>>>>> Stashed changes
     </div>
   );
 }
