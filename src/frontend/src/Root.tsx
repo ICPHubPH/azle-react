b37d385cb2e9cb68293d83965e7d24b4489da845
John Andrei Tacujan <89928907+DreiDev04@users.noreply.github.com>
2024-10-16 16:44:21 +0000

    CHANGED: Toggle to mode toggle dark mode

--- src/frontend/src/Root.tsx
+++ src/frontend/src/Root.tsx
@@ -1,21 +1,16 @@
 import { Outlet } from "react-router-dom";
 import ToggleSwitch from "./components/ui/ToggleSwitch";
 import { Navigation } from "./Navigation";
+import { ModeToggle } from "./components/ui/mode-toggle";
 
 const Layout = () => {
   return (
     <div className="h-screen">
-      {/* <header className=" min-h-12 flex items-center justify-center">
-        <nav className="flex gap-5">
-          <a href="/">Home</a>
-          <a href="/create">Create</a>
-          <a href="/login">Login</a>
-          <a href="/signup">Signup</a>
-        </nav>
-      </header> */}
       <Navigation>
         <Outlet />
-        <ToggleSwitch />
+        <div className="fixed bottom-5 right-5">
+          <ModeToggle />
+        </div>
       </Navigation>
     </div>
   );
