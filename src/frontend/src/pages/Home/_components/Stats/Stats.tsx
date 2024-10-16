7dae5d7fb21c72300efaff91f563eab12c8cd08a
Paula <peaksenut@gmail.com>
2024-10-16 11:58:20 +0000

    FIX: stats colors

--- src/frontend/src/pages/Home/_components/Stats/Stats.tsx
+++ src/frontend/src/pages/Home/_components/Stats/Stats.tsx
@@ -41,12 +41,14 @@ const Stats: React.FC<StatsProps> = ({ recalledForCount, id }) => {
     setRedoCardsLength(redoCardsLength + 1);
   }
 
+  const iconLabel = "leading-5 text-sm text-gray-500";
+
   return (
     <IconContext.Provider value={{ color: "gray", size: "2.1rem" }}>
-      <div className=" flex flex-col gap-2 sm:mb-5 font-sans mx-2">
+      <div className="flex flex-col gap-2 mb-3 sm:mb-5 font-sans mx-2">
         <StatsIcon onClick={onClickRecalledForCount}>
           {isRecalled ? <FaSquareCheck /> : <CiSquareCheck />}
-          <p className="leading-5 text-sm">
+          <p className={iconLabel}>
             Recalled <br /> {recalledForCount}&times;
           </p>
         </StatsIcon>
@@ -56,19 +58,19 @@ const Stats: React.FC<StatsProps> = ({ recalledForCount, id }) => {
             title="Hint"
           >
             <CiCircleQuestion />
-            <p className="leading-5 text-sm">Hint</p>
+            <p className={iconLabel}>Hint</p>
           </Modal>
         </StatsIcon>
         {!getIsRedo ? (
           <StatsIcon onClick={() => onRedo()}>
             <CiRedo />
-            <p className="leading-5 text-sm text-foreground">Redo</p>
+            <p className={iconLabel}>Redo</p>
           </StatsIcon>
         ) : null}
         <IconContext.Provider value={{ color: "gray", size: "1.7rem" }}>
           <span className="flex items-center justify-center flex-col mt-10">
             <PiStackSimpleLight />
-            <p className="leading-5 text-sm text-slate-500">
+            <p className={iconLabel}>
               ({redoCards.length}){" "}
               {redoCards.length <= 1 ? (
                 <>
