e0197847dafc84187f5fb1cf62623c20bb72ca22
jethrocadang <116892945+jethrocadang@users.noreply.github.com>
2024-10-18 04:09:26 +0800

    integration:[integrated posts  table in admin]

--- src/frontend/src/components/admin/AdminPostsTable.tsx
+++ src/frontend/src/components/admin/AdminPostsTable.tsx
@@ -3,12 +3,29 @@
 import { DataTable } from "@/components/tables/data-table";
 import { dummyPosts } from "@/components/data/dummy-data";
 import { postsColumnDefs } from "../tables/PostsColumnDef";
+import { useState } from "react";
+import { useAllPost } from "@/hooks/usePostData";
 
 export default function AdminPostsManagement() {
+  const [page, setPage] = useState(0);
+
+  const take = 10; // Number of items per page
+  const skip = page * take;
+
+  const { data, isLoading, isError } = useAllPost(skip, take);
+  const totalRows = data || 0;
+
   return (
     <div className="container mx-auto p-4 space-y-4 flex-grow">
       <h1 className="text-2xl font-bold mb-4">Admin Post Management</h1>
-      <DataTable columns={postsColumnDefs} data={dummyPosts} />
+      <DataTable
+        columns={postsColumnDefs}
+        data={data || []}
+        page={page}
+        pageSize={take}
+        totalRows={totalRows}
+        onPageChange={setPage}
+      />
     </div>
   );
 }
