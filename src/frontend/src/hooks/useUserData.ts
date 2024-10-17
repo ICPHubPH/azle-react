6159da7effed40b89af355333562a8fd39d6490f
jethrocadang <116892945+jethrocadang@users.noreply.github.com>
2024-10-18 04:04:48 +0800

    integration:[integrated users table in admin]

--- src/frontend/src/hooks/useUserData.ts
+++ src/frontend/src/hooks/useUserData.ts
@@ -4,11 +4,13 @@ import {
   deleteAccount,
   changeAvatar,
   updateSelf,
+  getAllStudents,
+  getAllProviders,
 } from "@/api/userService";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 // get ONE user
-export const useUser = (id: string) => {
+export const useUserById = (id: string) => {
   return useQuery({
     queryKey: ["user", id],
     queryFn: () => getUserById(id),
@@ -17,11 +19,36 @@ export const useUser = (id: string) => {
 };
 
 // get ALL users
-export const useUsers = () => {
+export const useAllUser = (skip: number, take: number) => {
   return useQuery({
-    queryKey: ["users"],
-    queryFn: getAllUsers,
+    queryKey: ["users", skip, take],
+    queryFn: () => getAllUsers(skip, take),
     staleTime: 10000,
+    refetchOnWindowFocus: false,
+    enabled: skip >= 0 && take > 0,
+  });
+};
+
+// get ALL STUDENTS
+export const useUserByStudents = (skip: number, take: number) => {
+  return useQuery({
+    queryKey: ["students", skip, take],
+    queryFn: () => getAllStudents(skip, take),
+    staleTime: 10000,
+    refetchOnWindowFocus: false,
+    enabled: skip >= 0 && take > 0,
+    select: (data) => ({ data: data.students, count: data.count }) // Return both data and count
+
+  });
+};
+//Providers
+export const useUserByProviders = (skip: number, take: number) => {
+  return useQuery({
+    queryKey: ["providers", skip, take],
+    queryFn: () => getAllProviders(skip, take),
+    staleTime: 10000,
+    refetchOnWindowFocus: false,
+    enabled: skip >= 0 && take > 0,
   });
 };
 
