1c25208a1c73dde6f8296d0f274d5b3e97a84de2
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-10-17 17:45:42 +0800

    Refactor student component to user component for better organization and readability for other developers; ensure responsiveness of the skeleton component

--- src/frontend/src/components/ui/skeleton.tsx
+++ src/frontend/src/components/ui/skeleton.tsx
@@ -14,14 +14,22 @@ function Skeleton({
 
 export function SkeletonCard() {
   return (
-    <div className="flex flex-col space-y-5">
-      <Skeleton className="h-[250px] w-[500px] rounded-xl" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+    <div className="flex flex-col space-y-3 overflow-hidden rounded-lg border  shadow-sm">
+      <Skeleton className="h-40 w-full" /> {/* Thumbnail */}
+      <div className="p-4 space-y-3">
+        <div className="flex items-center space-x-3">
+          <Skeleton className="h-10 w-10 rounded-full" /> {/* Avatar */}
+          <Skeleton className="h-4 w-1/2" /> {/* Provider name */}
+        </div>
+        <Skeleton className="h-4 w-full" /> {/* Description line 1 */}
+        <Skeleton className="h-4 w-2/3" /> {/* Description line 2 */}
+        <div className="flex justify-between items-center pt-2">
+          <Skeleton className="h-6 w-1/3" /> {/* Scholarship info */}
+          <Skeleton className="h-8 w-24 rounded-md" /> {/* Button */}
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
 export { Skeleton }
