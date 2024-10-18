4e1a9325f6f51758d6e96ad9c4803fc3326da9bf
Kurtd Daniel Bigtas <kuurtddbigtas@gmail.com>
2024-10-18 13:08:37 +0000

    chore: add a welcome message on backend

--- src/backend/start/routes.ts
+++ src/backend/start/routes.ts
@@ -1,13 +1,13 @@
-import AdminController from 'App/Controllers/Http/AdminController';
-import ApisController from 'App/Controllers/Http/ApisController';
-import AuthController from 'App/Controllers/Http/AuthController';
-import BookmarkController from 'App/Controllers/Http/BookmarkController';
-import FeedbackController from 'App/Controllers/Http/FeedbackController';
-import PostController from 'App/Controllers/Http/PostController';
-import UserController from 'App/Controllers/Http/UserController';
-import AuthMiddleware from 'App/Middlewares/Auth';
-import Pagination from 'App/Middlewares/Pagination';
-import { Router } from 'express';
+import AdminController from "App/Controllers/Http/AdminController";
+import ApisController from "App/Controllers/Http/ApisController";
+import AuthController from "App/Controllers/Http/AuthController";
+import BookmarkController from "App/Controllers/Http/BookmarkController";
+import FeedbackController from "App/Controllers/Http/FeedbackController";
+import PostController from "App/Controllers/Http/PostController";
+import UserController from "App/Controllers/Http/UserController";
+import AuthMiddleware from "App/Middlewares/Auth";
+import Pagination from "App/Middlewares/Pagination";
+import { Router } from "express";
 const Route = Router();
 
 /*
@@ -15,6 +15,8 @@ const Route = Router();
 | Public Routes
 |--------------------------------------------------------------------------
 */
+
+Route.get("/", ApisController.landing);
 
 Route.get("/greet", ApisController.greet);
 Route.get("/configurations", ApisController.configurations);
@@ -25,25 +27,32 @@ Route.post("/configuration/delete", ApisController.delete_configuration);
 Route.post("/auth/register", AuthController.register);
 Route.post("/auth/login", AuthController.login);
 Route.post("/auth/verify", AuthController.verify);
-Route.post('/auth/verify-register', AuthController.verifyFromRegister)
-Route.post('/auth/verify-login', AuthController.verifyFromLogin);
-Route.post('/auth/resend-otp', AuthController.resendOtp)
+Route.post("/auth/verify-register", AuthController.verifyFromRegister);
+Route.post("/auth/verify-login", AuthController.verifyFromLogin);
+Route.post("/auth/resend-otp", AuthController.resendOtp);
 
 Route.get("/users/:id", UserController.findById);
 Route.get("/users", Pagination.paginate, UserController.getAll);
-Route.get('/providers', Pagination.paginate, UserController.getProviders);
-Route.get('/non-verified-providers', Pagination.paginate, UserController.getNonVerifiedProviders);
-Route.get('/students', Pagination.paginate, UserController.getStudents);
+Route.get("/providers", Pagination.paginate, UserController.getProviders);
+Route.get(
+  "/non-verified-providers",
+  Pagination.paginate,
+  UserController.getNonVerifiedProviders
+);
+Route.get("/students", Pagination.paginate, UserController.getStudents);
 
-Route.get('/posts', Pagination.paginate, PostController.getAll);
-Route.get('/posts/:id', PostController.findById);
-Route.get('/posts/:id/category', PostController.findByCategorytype);
+Route.get("/posts", Pagination.paginate, PostController.getAll);
+Route.get("/posts/:id", PostController.findById);
+Route.get("/posts/:id/category", PostController.findByCategorytype);
 
-Route.post('/posts/:id/feedbacks', Pagination.paginate, FeedbackController.getPostFeedbacks);
-
+Route.post(
+  "/posts/:id/feedbacks",
+  Pagination.paginate,
+  FeedbackController.getPostFeedbacks
+);
 
 // testing email sending, change recipient in the controller
-Route.post('/test', UserController.test);
+Route.post("/test", UserController.test);
 
 /*
 |--------------------------------------------------------------------------
@@ -51,23 +60,72 @@ Route.post('/test', UserController.test);
 |--------------------------------------------------------------------------
 */
 
-Route.post("/users/:id/remove", AuthMiddleware.authorize, UserController.deleteById);
-Route.post("/@self/upload/valid-id", AuthMiddleware.authorize, UserController.uploadValidIdUrl); // TODO - add a middleware that verifies if the user is a provider
-Route.post("/@self/upload/avatar", AuthMiddleware.authorize, UserController.uploadAvatarUrl);
-Route.post("/@self/upload/banner", AuthMiddleware.authorize, UserController.uploadBannerUrl);
-Route.post('/@self/update', AuthMiddleware.authorize, UserController.updateSelf);
+Route.post(
+  "/users/:id/remove",
+  AuthMiddleware.authorize,
+  UserController.deleteById
+);
+Route.post(
+  "/@self/upload/valid-id",
+  AuthMiddleware.authorize,
+  UserController.uploadValidIdUrl
+); // TODO - add a middleware that verifies if the user is a provider
+Route.post(
+  "/@self/upload/avatar",
+  AuthMiddleware.authorize,
+  UserController.uploadAvatarUrl
+);
+Route.post(
+  "/@self/upload/banner",
+  AuthMiddleware.authorize,
+  UserController.uploadBannerUrl
+);
+Route.post(
+  "/@self/update",
+  AuthMiddleware.authorize,
+  UserController.updateSelf
+);
 
-Route.post('/users/:id/bookmarks', AuthMiddleware.authorize, Pagination.paginate, BookmarkController.getUserBookmarks);
-Route.post('/bookmarks', AuthMiddleware.authorize, BookmarkController.createBookmark);
-Route.post('/bookmarks/:id/remove', AuthMiddleware.authorize, BookmarkController.deleteBookmark);
+Route.post(
+  "/users/:id/bookmarks",
+  AuthMiddleware.authorize,
+  Pagination.paginate,
+  BookmarkController.getUserBookmarks
+);
+Route.post(
+  "/bookmarks",
+  AuthMiddleware.authorize,
+  BookmarkController.createBookmark
+);
+Route.post(
+  "/bookmarks/:id/remove",
+  AuthMiddleware.authorize,
+  BookmarkController.deleteBookmark
+);
 
-Route.post('/posts/create', AuthMiddleware.authorize, PostController.create);
-Route.post('/posts/:id', AuthMiddleware.authorize, PostController.updateById);
-Route.post('/posts/:id/remove', AuthMiddleware.authorize, PostController.deleteById);
+Route.post("/posts/create", AuthMiddleware.authorize, PostController.create);
+Route.post("/posts/:id", AuthMiddleware.authorize, PostController.updateById);
+Route.post(
+  "/posts/:id/remove",
+  AuthMiddleware.authorize,
+  PostController.deleteById
+);
 
-Route.post('/feedbacks', AuthMiddleware.authorize, FeedbackController.createFeedback);
-Route.post('/feedbacks/:id', AuthMiddleware.authorize, FeedbackController.updateFeedback);
-Route.post('/feedbacks/:id/remove', AuthMiddleware.authorize, FeedbackController.deleteFeedback);
+Route.post(
+  "/feedbacks",
+  AuthMiddleware.authorize,
+  FeedbackController.createFeedback
+);
+Route.post(
+  "/feedbacks/:id",
+  AuthMiddleware.authorize,
+  FeedbackController.updateFeedback
+);
+Route.post(
+  "/feedbacks/:id/remove",
+  AuthMiddleware.authorize,
+  FeedbackController.deleteFeedback
+);
 
 /*
 |--------------------------------------------------------------------------
@@ -75,17 +133,50 @@ Route.post('/feedbacks/:id/remove', AuthMiddleware.authorize, FeedbackController
 |--------------------------------------------------------------------------
 */
 
-Route.post('/protected/users/:id/archive', AuthMiddleware.authorize, AdminController.archiveUserById);
-Route.post('/protected/users/:id/unarchive', AuthMiddleware.authorize, AdminController.unarchiveUserById);
-Route.post('/protected/archived-users', Pagination.paginate, AuthMiddleware.authorize, AdminController.getArchivedUsers);
-Route.post('/protected/non-verified-users', Pagination.paginate, AuthMiddleware.authorize, AdminController.getNonVerifiedUsers);
+Route.post(
+  "/protected/users/:id/archive",
+  AuthMiddleware.authorize,
+  AdminController.archiveUserById
+);
+Route.post(
+  "/protected/users/:id/unarchive",
+  AuthMiddleware.authorize,
+  AdminController.unarchiveUserById
+);
+Route.post(
+  "/protected/archived-users",
+  Pagination.paginate,
+  AuthMiddleware.authorize,
+  AdminController.getArchivedUsers
+);
+Route.post(
+  "/protected/non-verified-users",
+  Pagination.paginate,
+  AuthMiddleware.authorize,
+  AdminController.getNonVerifiedUsers
+);
 
-Route.post('/protected/posts/:id/archive', AuthMiddleware.authorize, AdminController.archivePostById);
-Route.post('/protected/posts/:id/unarchive', AuthMiddleware.authorize, 
-    AdminController.unarchivePostById);
+Route.post(
+  "/protected/posts/:id/archive",
+  AuthMiddleware.authorize,
+  AdminController.archivePostById
+);
+Route.post(
+  "/protected/posts/:id/unarchive",
+  AuthMiddleware.authorize,
+  AdminController.unarchivePostById
+);
 
-Route.post('/protected/providers/:id/verify', AuthMiddleware.authorize, AdminController.verifyProvider);
-Route.post('/protected/non-verified-providers', Pagination.paginate, AuthMiddleware.authorize, AdminController.getNonVerifiedProviders);
+Route.post(
+  "/protected/providers/:id/verify",
+  AuthMiddleware.authorize,
+  AdminController.verifyProvider
+);
+Route.post(
+  "/protected/non-verified-providers",
+  Pagination.paginate,
+  AuthMiddleware.authorize,
+  AdminController.getNonVerifiedProviders
+);
 
 export { Route as routes };
-
