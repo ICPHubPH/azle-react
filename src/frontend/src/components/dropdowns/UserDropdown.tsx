7a63ec30ba8cfff365cec7b28d88f3ea8ded7475
ConnectED-hackathon <connected.mailer@gmail.com>
2024-10-19 21:35:59 +0000

    feat: resend otp authentication

--- src/frontend/src/components/dropdowns/UserDropdown.tsx
+++ src/frontend/src/components/dropdowns/UserDropdown.tsx
@@ -1,8 +1,15 @@
-import { useNavigate } from 'react-router-dom';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger, DropdownMenuLabel } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useAuth } from "@/hooks/use-auth";
 import { ChevronDown } from "lucide-react"; // Import the ChevronDown icon
-import { Separator } from '../ui/separator';
+import { useNavigate } from "react-router-dom";
+import { Separator } from "../ui/separator";
 
 interface User {
   name: string;
@@ -13,14 +20,15 @@ interface User {
 
 const UserDropdown: React.FC<{ user: User }> = ({ user }) => {
   const navigate = useNavigate();
+  const { logout } = useAuth();
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    navigate('/');
+    logout();
+    navigate("/");
   };
 
   return (
-    <DropdownMenu >
+    <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <div className="relative cursor-pointer ">
           {/* Avatar */}
@@ -30,35 +38,44 @@ const UserDropdown: React.FC<{ user: User }> = ({ user }) => {
           </Avatar>
 
           {/* ChevronDown icon positioned at the bottom-right of the Avatar */}
-          <ChevronDown 
-            className="absolute top-5 left-4 w-4 h-4 font-bold hover:text-blue-500 transition-colors text-white bg-slate-400 dark:bg-slate-900  rounded-full" 
-          />
+          <ChevronDown className="absolute top-5 left-4 w-4 h-4 font-bold hover:text-blue-500 transition-colors text-white bg-slate-400 dark:bg-slate-900  rounded-full" />
         </div>
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent align="end" className="w-full max-w-xs hidden md:flex flex-col ">
-      <div className="px-3 py-3 flex items-center gap-4">
+      <DropdownMenuContent
+        align="end"
+        className="w-full max-w-xs hidden md:flex flex-col "
+      >
+        <div className="px-3 py-3 flex items-center gap-4">
           <Avatar className="w-12 h-12">
             <AvatarImage src={user.avatarUrl} />
             <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
           </Avatar>
-          <div className=''>
-            <p className="text-sm font-semibold text-gray-900 dark:text-white">{user.name}</p>
-            <p className="text-xs text-gray-500 dark:text-gray-400">{user.email}</p>
-            <p className="text-xs text-gray-400 dark:text-gray-500">{user.role}</p>
+          <div className="">
+            <p className="text-sm font-semibold text-gray-900 dark:text-white">
+              {user.name}
+            </p>
+            <p className="text-xs text-gray-500 dark:text-gray-400">
+              {user.email}
+            </p>
+            <p className="text-xs text-gray-400 dark:text-gray-500">
+              {user.role}
+            </p>
           </div>
         </div>
-        <div className='pt-2'>
-        <Separator/>
+        <div className="pt-2">
+          <Separator />
         </div>
-        <DropdownMenuLabel  />
+        <DropdownMenuLabel />
 
-        <DropdownMenuItem onSelect={() => { navigate('/profile'); }}>
+        <DropdownMenuItem
+          onSelect={() => {
+            navigate("/profile");
+          }}
+        >
           Profile
         </DropdownMenuItem>
-        <DropdownMenuItem onSelect={handleLogout}>
-          Logout
-        </DropdownMenuItem>
+        <DropdownMenuItem onSelect={handleLogout}>Logout</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
